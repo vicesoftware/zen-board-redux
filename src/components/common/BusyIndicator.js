be2ea1400304d6dabfb79871f882a4266822f5d5
Ryan Vice <ryanvice@Ryans-MacBook-Pro.local>
2016-07-17 15:49:12 -0500

    Updated busy spinner to be annimated font awesome icon

--- src/components/common/BusyIndicator.js
+++ src/components/common/BusyIndicator.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const BusyIndicator = () => {
-	return (
-    <h3>Loading...</h3>
-	);
+  return (
+    <div>
+      <i className="fa fa-spinner fa-pulse fa-3x fa-fw text-muted"></i>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
 };
 
 export default BusyIndicator;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Router, browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './styles/styles.css';
 
 const store = configureStore();
