c4ab7c19efd6ecf07768ef31bc9ffcbe0edbfd34
Ryan Vice <ryan_andrus@dellteam.com>
2016-07-29 10:45:50 -0500

    Adding component index files

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app/App';
-import ProjectsPage from './components/projects/components/ProjectsPage';
+import projects from './components/projects/';
 import ManageProjectsPage from './components/manageProject/ManageProjectPage';
-import ProjectPage from './components/project/ProjectPage';
+import ProjectPage from './components/project/components/ProjectPage';
 
 export default (
 	<Route path="/" component={App}>
-		<IndexRoute component={ProjectsPage}/>
+		<IndexRoute component={projects.components.ProjectsPage}/>
 		<Route path="projects/add" component={ManageProjectsPage}/>
 		<Route path="projects/edit/:id" component={ManageProjectsPage}/>
 		<Route path="projects/:id" component={ProjectPage}/>
